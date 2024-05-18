0f3dd23b16e825002a03f4c3c8e298354a20a563
voskreseniev <roman.mayber@gmail.com>
2024-05-18 23:52:54 +0300

    Update pypy.py

--- pypy.py
+++ pypy.py
@@ -22,3 +22,64 @@ for process in processes:
     process.wait()
 
 print("Все зависимости успешно установлены.")
+import subprocess
+import concurrent.futures
+import shutil
+import os
+
+dependencies = [
+    "keyboard",
+    "psutil",
+    "pyautogui",
+    "opencv-python",
+    "numpy",
+    "pywin32",
+    "pyscreeze",
+    "Pillow"
+]
+
+# Установка зависимостей
+for dep in dependencies:
+    subprocess.run(["pip", "install", dep], check=True)
+
+print("Все зависимости успешно установлены.")
+
+# Выполнение дополнительных команд
+additional_commands = [
+    "pyinstaller --onefile set.py",
+    "pyinstaller --onefile pnz.py",
+    "pyinstaller --onefile cfg.py"
+]
+
+def execute_command(command):
+    subprocess.run(command, shell=True, check=True)
+
+# Используем ThreadPoolExecutor для выполнения команд параллельно
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(execute_command, additional_commands)
+
+print("Дополнительные команды выполнены.")
+
+# Определение путей к папкам, которые нужно скопировать
+source_folders = [
+    "img",
+    "Config by Danze"
+]
+
+# Определение путей к исходной и целевой папкам
+source_directory = os.path.dirname(os.path.abspath(__file__))  # Путь к текущей папке скрипта
+target_directory = os.path.join(source_directory, "dist")
+
+# Создание целевой папки, если она не существует
+os.makedirs(target_directory, exist_ok=True)
+
+# Копирование папок
+for folder in source_folders:
+    source_path = os.path.join(source_directory, folder)
+    target_path = os.path.join(target_directory, folder)
+    try:
+        shutil.copytree(source_path, target_path)
+    except FileExistsError:
+        print(f"Папка {target_path} уже существует.")
+
+print("Папки успешно скопированы.")
